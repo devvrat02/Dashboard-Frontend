11e9f4a3a44123dcb737f9f46629a33d958f95b5
Chetan Malviya <malviyachetan02@gmail.com>
2023-08-06 18:11:55 +0530

    server

--- src/api/server.tsx
+++ src/api/server.tsx
@@ -1,6 +1,6 @@
 
 // let localhost = "http://localhost:8080";
-let localhost = "https://dashboard-backend-7eq9qc0hl-devvrat02.vercel.app/";
+let localhost = "https://dashboard-backend-7eq9qc0hl-devvrat02.vercel.app";
 const baseUrl = localhost + "/api/v1";
 
 export default baseUrl;
